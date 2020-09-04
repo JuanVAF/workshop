2247ee1ede1049c139e52f1170821e717cc8207c
Jason Volney Silva <jasonvolneysilva@gmail.com>
2020-09-04 15:10:48 -0300

    Publishing site on github pages

---
+++ precache-manifest.69b5d2e6a16da15174ca7a413abc98f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed63ec3e6390f21d8182bdab74dddde1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f8101d21681a7d3c4c",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "690731d473c5598ae760",
+    "url": "/static/css/main.930a2062.chunk.css"
+  },
+  {
+    "revision": "b0f8101d21681a7d3c4c",
+    "url": "/static/js/2.9716c11a.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.9716c11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690731d473c5598ae760",
+    "url": "/static/js/main.9054cad1.chunk.js"
+  },
+  {
+    "revision": "c638b4f4537bd4822f41",
+    "url": "/static/js/runtime-main.1032e247.js"
+  }
+]);
